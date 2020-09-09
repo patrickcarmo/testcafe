c1f488efb965b42d87eb2cbcdfa17abb29d1e7a5
Patrick Carmo <patrick.carmo@wolterskluwer.com>
2020-09-09 11:00:33 +0100

    testing feedback

---
+++ tests/feedback.test.js
@@ -0,0 +1,25 @@
+import { Selector, t } from 'testcafe';
+
+fixture('Feedback form test').page('http://zero.webappsecurity.com/index.html');
+
+test('User can submit feedback via form', async (tc) => {
+  // Selectors
+  const linkToFeedback = Selector('#feedback');
+  const formName = Selector('#name');
+  const formEmail = Selector('#email');
+  const formSubject = Selector('#subject');
+  const formComment = Selector('#comment');
+  const submitButton = Selector('input').withAttribute('value', 'Send Message');
+  const message = Selector('div').innerText;
+
+  // Actions
+  await tc.click(linkToFeedback);
+  await tc.typeText(formName, 'NAME', { paste: true });
+  await tc.typeText(formEmail, 'email@random.com', { paste: true });
+  await tc.typeText(formSubject, 'SUBJECT', { paste: true });
+  await tc.typeText(formComment, 'COMMENT', { paste: true });
+  await tc.click(submitButton);
+
+  // Assertion
+  await t.expect(message).contains('Thank you for your comments');
+});
