3b33ff39713318c80fd2e39ce28a26fd20cd3d57
Patrick Carmo <patrick.carmo@wolterskluwer.com>
2020-09-09 12:12:43 +0100

    refactored to use login helper to login to application

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,50 +1,28 @@
 import { Selector } from 'testcafe';
+import { login } from '../helpers';
 
 fixture('Login Test').page('http://zero.webappsecurity.com/index.html');
 
 test('User cannot login with invalid credentials', async (tc) => {
-  const signInButton = Selector('#signin_button');
-  await tc.click(signInButton);
-
-  const loginForm = Selector('#login_form');
-  await tc.expect(loginForm.exists).ok();
-
-  const userLogin = Selector('#user_login');
-  const userPassword = Selector('#user_password');
-  await tc.typeText(userLogin, 'invalid login', { paste: true });
-  await tc.typeText(userPassword, 'invalid password', { paste: true });
-
-  const submitButton = Selector('input[type=submit]');
-  await tc.click(submitButton);
-
+  // Selectors
   const alertError = Selector('.alert.alert-error');
+
+  // Actions
+  await login('userName', 'password');
+
+  // Assertion
   await tc.expect(alertError.exists).ok();
 });
 
 test('User can login into application', async (tc) => {
-  const signInButton = Selector('#signin_button');
-  await tc.click(signInButton);
-
+  // Selectors
   const loginForm = Selector('#login_form');
-  await tc.expect(loginForm.exists).ok();
-
-  const userLogin = Selector('#user_login');
-  const userPassword = Selector('#user_password');
-  await tc.typeText(userLogin, 'username', { paste: true });
-  await tc.typeText(userPassword, 'password', { paste: true });
-
-  const submitButton = Selector('input[type=submit]');
-  await tc.click(submitButton);
-
   const accountSummaryTab = Selector('#account_summary_tab');
+
+  // Actions
+  await login('username', 'password');
+
+  // Assertion
   await tc.expect(accountSummaryTab.exists).ok();
   await tc.expect(loginForm.exists).notOk();
-
-  const iconUser = Selector('.icon-user');
-  await tc.click(iconUser);
-
-  const logoutLink = Selector('#logout_link');
-  await tc.click(logoutLink);
-  await tc.expect(logoutLink.exists).notOk();
-  await tc.expect(signInButton.exists).ok();
 });
--- tests/new-payee.test.js
+++ tests/new-payee.test.js
@@ -1,17 +1,10 @@
 import { Selector, t } from 'testcafe';
+import { login } from '../helpers';
 
 fixture('New payee test').page('http://zero.webappsecurity.com/index.html');
 
 test.before(async (tc) => {
-  const signInButton = Selector('#signin_button');
-  const userLogin = Selector('#user_login');
-  const userPassword = Selector('#user_password');
-  const submitButton = Selector('input[type=submit]');
-
-  await tc.click(signInButton);
-  await tc.typeText(userLogin, 'username', { paste: true });
-  await tc.typeText(userPassword, 'password', { paste: true });
-  await tc.click(submitButton);
+  await login('username', 'password');
 })('User can add new payee to the list', async (tc) => {
   // Selectors
   const payBillsTab = Selector('#pay_bills_tab');
