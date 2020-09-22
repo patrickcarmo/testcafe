427fdd06dc43fd6d4b22894cad7a6b4ba179c5c9
Patrick Carmo <patrick.carmo@wolterskluwer.com>
2020-09-22 11:21:26 +0100

    updated properties

--- page-objects/pages/LoginPage.js
+++ page-objects/pages/LoginPage.js
@@ -2,9 +2,12 @@ import { Selector, t } from 'testcafe';
 
 class LoginPage {
   constructor() {
+    this.loginForm = Selector('#login_form');
     this.userLogin = Selector('#user_login');
     this.userPassword = Selector('#user_password');
     this.submitButton = Selector('.btn-primary');
+    this.errorMessage = Selector('.alert.alert-error');
+    this.forgottenPassword = Selector('a').withText('Forgot your password ?');
   }
 
   async loginToApp(username, password) {
